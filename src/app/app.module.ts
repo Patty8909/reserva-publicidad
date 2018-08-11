b12d10a02eacea4087e3911b9e0cd59839a2ef38
wendytamara <anacarlavegam@gmail.com>
2018-08-11 14:36:41 -0500

    añadiendo maquetado de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MaterializeModule } from 'angular2-materialize';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
